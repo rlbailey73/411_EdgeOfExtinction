16290617f9a74d896f1d15e99b3edbbb6d0d8b47
AnthonyBoggio <a.p.boggio@eagle.clarion.edu>
2018-02-12 18:08:43 -0500

    Add files via upload

---
+++ Assets/Scripts/ChangeScenes.cs
@@ -0,0 +1,18 @@
+﻿//WILL BE USED TO CHANGE THE SCENE WHENEVER NECESSARY
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScenes : MonoBehaviour {
+
+    //NO NEED FOR START OR UPDATE FUNCTIONS
+
+    // Changes to the appropriate scene
+    public void ChangeScene(string sceneName)
+    {
+        //Application.LoadLevel(sceneName);
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ Assets/Scripts/PickDeck.cs
@@ -0,0 +1,39 @@
+﻿//WILL DETERMINE WHICH DECK IS PICKED AND WILL DISPLAY THE CARDS ABSED ON THAT DECK
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickDeck : MonoBehaviour {
+
+    public int cr = 0; //clarion river deck
+    public int anp = 1; //allagheny national park deck
+    public int ah = 2; //appalachian homestead deck
+    public int pb = 3; //peat bogs deck
+
+    public int chosenDeck = -1; //set to -1 to determine if there are any issues
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //sets the deck chosen
+    public void SetDeck(int deck)
+    {
+        chosenDeck = deck;
+    }
+
+    //Determines which deck is selected
+    public void ChosenDeck()
+    {
+
+    }
+}
